a2f43402a8471b0e41f7bbb924119ca7220f2720
PyYoshi <myoshi321go@gmail.com>
2016-07-17 16:20:51 +0900

    add errors

---
+++ errors.go
@@ -0,0 +1,12 @@
+package unity
+
+import "errors"
+
+// ErrInvalidAssetBundleType 不正なAssetBundle形式
+var ErrInvalidAssetBundleType = errors.New("Invalid AssetBundle type")
+
+// ErrUnsupportedPlayerVersion サポート外のPlayer Version
+var ErrUnsupportedPlayerVersion = errors.New("Unsupported player version")
+
+// ErrUnsupportedCompressionType サポート外の圧縮形式
+var ErrUnsupportedCompressionType = errors.New("Unsupported compression type")
